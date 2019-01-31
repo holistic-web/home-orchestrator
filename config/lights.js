7c83c88d6d37f54947a99f938c5fe3aaa9cc1d1d
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-01-31 11:49:54 +0000

    feat: update config file

--- config/lights.js
+++ config/lights.js
@@ -1,28 +1,14 @@
-require('dotenv').config()
-
-export default {
-	nanoleaf: {
-		type: 'nanoleaf',
-		endpoints: {
-			setBrightness: process.env.NANOLEAF_SET_BRIGHTNESS_ENDPOINT || false,
-			turnOff: process.env.NANOLEAF_TURN_OFF_ENDPOINT || false,
-			setScene: process.env.NANOLEAF_SET_SCENE_ENDPOINT || false
-		}
+export default [
+	{
+		name: 'nanoleaf',
+		type: 'nanoleaf'
 	},
-	room: {
-		type: 'hue',
-		endpoints: {
-			setBrightness: process.env.ROOM_SET_BRIGHTNESS_ENDPOINT || false,
-			turnOff: process.env.ROOM_TURN_OFF_ENDPOINT || false,
-			setScene: process.env.ROOM_SET_SCENE_ENDPOINT || false
-		}
+	{
+		name: 'room',
+		type: 'hue'
 	},
-	lamp: {
-		type: 'hue',
-		endpoints: {
-			setBrightness: process.env.LAMP_SET_BRIGHTNESS_ENDPOINT || false,
-			turnOff: process.env.LAMP_TURN_OFF_ENDPOINT || false,
-			setScene: process.env.LAMP_SET_SCENE_ENDPOINT || false
-		}
+	{
+		name: 'lamp',
+		type: 'hue'
 	}
-}
+]
