af8a37f1b8cfed71dfed1edefd595e3e38bc27da
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-02-05 23:36:01 +0000

    chore: refactor

--- functions/controllers/ButtonController.js
+++ functions/controllers/ButtonController.js
@@ -5,16 +5,10 @@ const cors = require('cors');
 const url = require('url');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const firebaseConfig = require('../config/firebase');
+const { toggleLight, toggleAllLights } = require('../lib/StateService');
 
-admin.initializeApp({
-	// #Todo: find a way to use the values from @/config/firebase.js
-	apiKey: "AIzaSyCvBMLoUZafodd2vlyAxUs3Pl0mEARUOEc",
-	authDomain: "holistic-home-5134d.firebaseapp.com",
-	databaseURL: "https://holistic-home-5134d.firebaseio.com",
-	projectId: "holistic-home-5134d",
-	storageBucket: "holistic-home-5134d.appspot.com",
-	messagingSenderId: "549410338560"
-});
+admin.initializeApp(firebaseConfig);
 database = admin.database();
 
 const app = express();
@@ -34,53 +28,6 @@ const validateToken = (req, res, next) => {
 
 // Add middleware to authenticate requests
 app.use(validateToken);
-
-const isLightOff = async (lightName) => {
-	const ref = database.ref(`lights/${lightName}/off`);
-	const snap = await ref.once('value');
-	const value = snap.val();
-	return value;
-}
-
-/**
- * If a light has a non 0 brightness it sets it to 0. Otherwise it sets brightness to 100.
- * @param {string} lightName name of the light you wish to toggle
- */
-const toggleLight = async (lightName) => {
-	const isOff = await isLightOff(lightName);
-	if (isOff) {
-		database.ref(`lights/${lightName}/off`).set(false);
-		return true;
-	}
-	database.ref(`lights/${lightName}/off`).set(true);
-	return false;
-}
-
-/**
- * If any light is on it sets everything to 100%. Otherwise sets everything to 0
- */
-const toggleAllLights = async () => {
-	const [lampIsOff, roomIsOff, nanoleafIsOff] = await Promise.all([
-		isLightOff('lamp'),
-		isLightOff('room'),
-		isLightOff('nanoleaf')
-	]);
-	const isAnythingOn = !lampIsOff || !roomIsOff || !nanoleafIsOff;
-	if (isAnythingOn) {
-		await database.ref('lights').update({
-			'lamp/off': true,
-			'room/off': true,
-			'nanoleaf/off': true
-		});
-	} else {
-		await database.ref('lights').update({
-			'lamp/off': false,
-			'room/off': false,
-			'nanoleaf/off': false
-		});
-	}
-	return isAnythingOn;
-}
 
 // build multiple CRUD interfaces:
 app.post('/desk-button/short-press', async (req, res) => {
