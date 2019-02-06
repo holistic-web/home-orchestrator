e0ad03a27808466ea3b2df084a5a4143174bef73
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-02-06 14:45:03 +0000

    -

--- functions/config/index.js
+++ functions/config/index.js
@@ -2,6 +2,5 @@
 const functions = require('firebase-functions');
 
 module.exports = {
-	// IFTT_KEY: functions.config().ifttt.key
-	IFTT_KEY: 'bbv-N1ZXD5k_X8093_ZrlR'
+	IFTT_KEY: functions.config().ifttt.key
 }
