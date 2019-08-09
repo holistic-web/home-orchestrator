db45e8a50d4dfb5eb1f926fbe16dd7c998fd2651
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2019-08-09 21:25:01 +0100

    chore: update logs

--- orchestration-api/app.js
+++ orchestration-api/app.js
@@ -30,5 +30,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+  console.log(`Orchestration API listening on port ${process.env.PORT}!`),
 );
