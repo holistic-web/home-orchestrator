2e6a491c037fc219b96bc4a2e2f48fa9cdeccf2e
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2019-08-10 02:03:11 +0100

    feat: enable async request handling

--- orchestration-api/app.js
+++ orchestration-api/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const admin = require('firebase-admin');
 const serviceAccount = require('./lib/service-account.json');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
+const ErrorHandlerMiddleware = require('./middlewares/ErrorHandler');
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
@@ -12,7 +14,11 @@ admin.initializeApp({
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/lights', routes.LightController)
+
+app.use(ErrorHandlerMiddleware);
 
 app.listen(process.env.PORT, () =>
   console.log(`Orchestration API listening on port ${process.env.PORT}!`),
--- orchestration-api/modules/lights/LightController.js
+++ orchestration-api/modules/lights/LightController.js
@@ -5,33 +5,35 @@ const LightService = require('./LightService');
 const router = Router();
 const lightService = new LightService();
 
-router.post('/update', async (req, res) => {
-	console.log('req: ', req);
-	console.log('req.body: ', req.body);
-	console.log('req.data: ', req.data);
+router.post('/update', async (req, res, next) => {
+	try {
+		console.log(req);
+		const { lights, networkId } = req.body;
+		console.log('> lights/update~ called with: ' + JSON.stringify({ lights, networkId }, null, 4));
 
-	const { lights, networkId } = req.body;
-	console.log('> lights/update~ called with: ' + JSON.stringify({ lights, networkId }, null, 4));
+		// Update the Database
+		const batchWrite = admin.firestore().batch();
+		lights.forEach(light => {
+			lightRef = admin.firestore().collection('networks').doc(networkId).collection('lights').doc(light._id);
+			batchWrite.update(lightRef, light);
+		});
+		await batchWrite.commit();
 
-	// Update the Database
-	const batchWrite = admin.firestore().batch();
-	lights.forEach(light => {
-		lightRef = admin.firestore().collection('networks').doc(networkId).collection('lights').doc(light._id);
-		batchWrite.update(lightRef, light);
-	});
-	await batchWrite.commit();
+		// Get the IFTTT key
+		const networkDoc = admin.firestore().collection('networks').doc(networkId);
+		const networkSnap = await networkDoc.get();
+		const network = networkSnap.data();
+		const IFTTT_KEY = network.variables.IFTTT_KEY;
 
-	// Get the IFTTT key
-	const networkDoc = admin.firestore().collection('networks').doc(networkId);
-	const networkSnap = await networkDoc.get();
-	const network = networkSnap.data();
-	const IFTTT_KEY = network.variables.IFTTT_KEY;
+		// Update the lights
+		console.log('> updateLights~ updating the lights\' state');
+		await lightService.updateLights(lights, IFTTT_KEY);
 
-	// Update the lights
-	console.log('> updateLights~ updating the lights\' state');
-	await lightService.updateLights(lights, IFTTT_KEY);
+		return res.send('done');
 
-	return res.send();
+	} catch (err) {
+		next(err);
+	}
 });
 
 module.exports = router;
