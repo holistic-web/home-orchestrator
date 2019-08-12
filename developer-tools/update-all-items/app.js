c17abc9222c03efe7fb6eec6b62fcc617c8b382e
Andrew Lewis <39633492+andrewlew1s@users.noreply.github.com>
2019-08-12 15:40:42 -0700

    Update app.js
    mike made me do this

--- developer-tools/update-all-items/app.js
+++ developer-tools/update-all-items/app.js
@@ -2,8 +2,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./service-account.json');
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: 'https://holistic-home-5134d.firebaseio.com'
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://holistic-home-5134d.firebaseio.com'
 });
 
 // #TODO: set this value to the collection you want to modify
@@ -12,33 +12,44 @@ const collection = admin.firestore().collection(collectionName);
 
 const updateItems = async () => {
 
-	// Get the items
-	console.log('> Getting items from the database...');
-	const snapshots = await collection.get();
-	const items = snapshots.docs.map(doc => doc.data());
+    // Get the items
+    console.log('> Getting items from the database...');
+    const snapshots = await collection.get();
+    const items = snapshots.docs.map(doc => doc.data());
 
-	// Modify the items
-	console.log('> Modifying the data...')
-	const promises = items.map(async item => {
+    // Modify the items
+    console.log('> Modifying the data...')
+    const promises = items.map(async item => {
 
-		// #TODO edit the logic below to modify the items
-		if (item) {
-			Console.log(`> Working on: ${JSON.stringify(item, null, 4)}...`);
-			item.newProperty = 'example';
+        // #TODO edit the logic below to modify the items
+        if (!item.settings) {
+            Console.log(`> Working on: ${JSON.stringify(item, null, 4)}...`);
+            item.newProperty = 'example';
 
-			// Update the item
-			const document = collection.doc(item._id);
-			await document.set(item);
-		}
+            item.settings = {
+                IFTTT: { key: item.variables.IFTTT_KEY },
+                lights: {
+                    hue: {
+                        usingLocalAPI: false
+                    }
+                }
+            }
 
-	});
+            delete item.variables
 
-	// Wait for updates to be applied
-	console.log('> Applying updates...');
-	await Promise.all(promises);
+            // Update the item
+            const document = collection.doc(item._id);
+            await document.set(item);
+        }
 
-	console.log('Done!');
+    });
+
+    // Wait for updates to be applied
+    console.log('> Applying updates...');
+    await Promise.all(promises);
+
+    console.log('Done!');
 
 };
 
-updateItems();
+updateItems()
