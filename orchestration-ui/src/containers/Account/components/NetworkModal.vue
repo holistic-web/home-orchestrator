f0cb10dc94751c619a4144241714e0fc66b3d2f5
dan47bennett <dghbennett47@gmail.com>
2019-08-18 20:28:04 +0100

    feat: place active network table within modal

---
+++ orchestration-ui/src/containers/Account/components/NetworkModal.vue
@@ -0,0 +1,38 @@
+<template>
+	<b-modal
+		:visible="visible"
+		title="Choose Active Network"
+		ok-variant="outline-info"
+		ok-title="Save"
+		:ok-disabled="isSubmitDisabled"
+		@ok="onSubmit"
+		@hidden="$emit('hidden')">
+
+			<span v-if="isSubmitting" v-text="'Submitting...'"/>
+
+			<template v-if="!isSubmitting">
+
+				<b-table
+					v-else
+					:items="networks"
+					:fields="tableFields">
+
+					<template slot="users" slot-scope="data">
+						{{data.item.users.length + 1}}
+					</template>
+
+					<template slot="actions" slot-scope="data">
+						<b-button
+							variant="outline-primary"
+							size="sm"
+							v-text="'Set Active'"
+							:disabled="isSetNetworkActiveDisabled(data.item)"
+							@click="onNetworkSelect(data.item)"/>
+					</template>
+
+				</b-table>
+
+			</template>
+
+	</b-modal>
+</template>
